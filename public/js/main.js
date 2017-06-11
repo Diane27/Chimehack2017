6ae28744944cb9a0cf683c7ad18a1c932e7d9fa8
Taewoo Kim <kimtaewoo8554@gmail.com>
2017-06-11 09:38:19 -0700

    changes
    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
+var activeTab = document.querySelector('.active');
+var inactiveTab = document.querySelectorAll('.inactive');
+
+
+inactiveTab[0].onclick = function() {
+
+       inactiveTab[0].classList.add('active');
+       activeTab.classList.remove('active');
+       inactiveTab[1].classList.remove('active');
+}
+
+inactiveTab[1].onclick = function() {
+
+       inactiveTab[1].classList.add('active');
+       activeTab.classList.remove('active');
+       inactiveTab[0].classList.remove('active');
+}
+
+activeTab.onclick = function() {
+
+       inactiveTab[0].classList.remove('active');
+       activeTab.classList.add('active');
+       inactiveTab[1].classList.remove('active');
+}
+
 $(document).ready(function(){
    $('[data-toggle="offcanvas"]').click(function(){
        $("#navigation").toggleClass("hidden-xs");
